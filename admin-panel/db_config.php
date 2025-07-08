310e98c56f6713e4cbf6b452500c648db6cabdf7
yatish jobanputa <jobanputrayatish@gmail.com>
2025-07-08 21:44:17 +0530

    Update db_config.php

--- admin-panel/db_config.php
+++ admin-panel/db_config.php
@@ -2,7 +2,7 @@
 $host = "localhost";       // usually localhost
 $username = "root";        // your MySQL username (default is 'root' in XAMPP)
 $password = "";            // your MySQL password (empty by default in XAMPP)
-$database = "majedar";     // your database name
+$database = "database966";     // your database name
 
 $conn = new mysqli($host, $username, $password, $database);
 
