305268d318157b379201991cc62374b01859b810
yatish jobanputa <jobanputrayatish@gmail.com>
2025-07-08 21:46:12 +0530

    Update admin_layout.php

--- admin-panel/code-files/admin_layout.php
+++ admin-panel/code-files/admin_layout.php
@@ -7,7 +7,7 @@ require_once('auth_check.php');
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="icon" type="images/logo.png" href="fav.ico">
-  <title>majedar Admin</title>
+  <title>Admin area</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
   <style>
